9cb44bf9948f789629e3c7164c84feba0961a76f
Rahmatullah Nikyar <rahmatullahnikyar@Rahmatullahs-MacBook-Pro.local>
2018-12-04 21:04:25 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -2,26 +2,59 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+var ContactDictionary = [
+                          {name:"Rahm Nikyar", phone:4164164166},
+                          {name:"Ellen Choi", phone:6476476477},
+                          {name:"Bilal Ahmed", phone:9059059055},
+                          {name:"Brian Yang", phone:3053053055}
+                        ]
+
+
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render()  {
+    return <div> {this.props.name} </div>
+  }
+}
+
+class ContactList extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      contacts:[],
+    }
+  }
+
+  generateContacts() {
+    let contacts = [];
+
+    for (let i=0; i < ContactDictionary.length; i++) {
+      contacts.push(<Contact name={ContactDictionary[i].name}/>)
+    }
+    return contacts
+  }
+
   render() {
+    var contactwidgets = this.generateContacts();
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div class="contact-list" id={this.props.list}>
+      {contactwidgets}
       </div>
-    );
+
+      )
+  }
+
+}
+
+class App extends Component {
+
+
+  render() {
+    return <ContactList list="Checked In"/>;
   }
 }
 
