f16d01fc024333b80765249c9f5b539f8d2fada6
Bilal Ahmed <bill.ahmed@mail.utoronto.ca>
2019-03-04 22:30:47 -0500

    many UI changes for contactCard and other styling improvements

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -8,18 +8,22 @@ class ContactCard extends Component {
     let contacts = require('../ContactInfo.json');
     let employee = {
         "id": "",
-        "name": [],
+        "name": "",
+        "title": "",
+        "location": "",
         "phoneNumber": [],
-        "email": []
+        "email": [],
     };
 
     // Find the current user by ID and fill in their info
     for(var i = 0; i < contacts.length; i++){
       if(contacts[i]["B"] === this.props.match.params.id){
         employee["id"] = this.props.match.params.id;
+        employee["location"] = contacts[i]["A"];
         employee["name"] = contacts[i]["B"];
         employee["email"] = contacts[i]["C"];
         employee["phoneNumber"] = contacts[i]["D"];
+        employee["title"] = contacts[i]["K"];
       }
     }
 
@@ -30,17 +34,32 @@ class ContactCard extends Component {
 }
 
 	render() {
-    console.log(contactInformation);
-        let contactInformation = this.state.employee;
-	    return (
-	      <div className="ContactCard">
-          <h2>Employee ID: {this.state.employee_id}</h2>
-          <h2>Employee Name: {contactInformation["name"]}</h2>
-          <h2>Employee Number: {contactInformation["name"]}</h2>
-          <h2>Employee Email: {contactInformation["email"]}</h2>
-	      </div>
-	    );
-	  }
+    let contactInformation = this.state.employee;
+    return (
+      <div className="ContactCard">
+
+        <div className="contactOverview">
+          <img src={require("./assets/default_profile_pic.png")} height="35%" width="35%"/>
+          <h2 className="employeeName">{contactInformation["name"]}</h2>
+          <h3 className="employeeTitle"><i> {contactInformation["title"]} </i></h3>
+        </div>
+
+        <div className="detailedInfo">
+          <div className="phoneInfo">
+            <h2>{contactInformation["phoneNumber"]}</h2>
+          </div>
+
+          <div className="emailInfo">
+            <h2>{contactInformation["email"]}</h2>
+          </div>
+
+          <div className="locationInfo">
+            <h2>{contactInformation["location"]}</h2>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ContactCard;
