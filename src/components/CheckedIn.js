6a14a58a8a6ac4a17075634215fa5a04f6979cb7
Bilal Ahmed <bill.ahmed@mail.utoronto.ca>
2019-03-30 13:43:48 -0400

    fixed issues with using List API + added profile pic and routing to contactCard

--- src/components/CheckedIn.js
+++ src/components/CheckedIn.js
@@ -2,9 +2,8 @@ import React, { Component } from 'react';
 import './CheckedIn.css';
 import Button from '@material-ui/core/Button';
 import Avatar from '@material-ui/core/Avatar';
-import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import { ListItemText, ListItemIcon, ListItemAvatar, ListItemSecondaryAction, IconButton } from '@material-ui/core';
+import {ListItemText} from '@material-ui/core';
 import {
   BrowserRouter as Router,
   Link,
@@ -27,45 +26,38 @@ class CheckedIn extends Component {
 	}
 
 	render() {
-		let new_url = "/contactCard/" + this.props.text;
-		//<Link to={new_url}> {this.props.text} </Link>
+		let new_url = "/contactCard/" + this.props.text;			
+		
 	    return (
 
 	      <div className="CheckedIn">
-					<List>
-							<ListItem button alignItems="center" onClick={this.handleClick}>
+					<div className="userName">
+					<Link to={new_url}>
+							<ListItem button alignItems="center">
+								<Avatar>
+                  <img className="userProfilePic" src={require("./assets/default_profile_pic.png")}/>
+                </Avatar>
 								<ListItemText primary={this.props.text}></ListItemText>
+							</ListItem>
+					</Link>
+					</div>
+					<div className="buttons">
 
-								<ListItemSecondaryAction>
-								<button className="btnContactPhone"
+					<button className="btnContactPhone"
 									name="viewContactPhone"
 									type="button"
 									href="tel:416-456-7890"
 									/>
-									
-									<button className="btnContactSMS"
+					
+					<button className="btnContactSMS"
 									name="viewContactSMS"
 									type="button"
 									href="sms:416-456-7890"
 									/>
-									<Button className="btnCheckIn" variant="contained" color="primary" >Check In
-									</Button>
-								</ListItemSecondaryAction>
 
-								<div className="buttons">
-									
+					<Button className="btnCheckIn" variant="contained" color="primary" >Check In</Button>
 
-									
-
-									{/* <button
-										className="btnContactInfo"
-										name="viewContactInfo"
-										type="button"
-										>
-									</button> */}
-									</div>
-							</ListItem>
-					</List>
+					</div>
 	      </div>
 	    );
 	  }
