c5589f7e5ba2e51c60bf3ea72a73a85081027c9c
Bilal Ahmed <bill.ahmed@mail.utoronto.ca>
2019-04-24 10:07:53 -0400

    Updates

---
+++ precache-manifest.778674fdc8b6038a33e6125c8c73d86a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "46d9e2e783c1cbb980ce",
+    "url": "/DDSBlaze/static/css/main.58425d99.chunk.css"
+  },
+  {
+    "revision": "46d9e2e783c1cbb980ce",
+    "url": "/DDSBlaze/static/js/main.a41b3cc3.chunk.js"
+  },
+  {
+    "revision": "7d6c3ac2a19008394b18",
+    "url": "/DDSBlaze/static/js/runtime~main.36fba92d.js"
+  },
+  {
+    "revision": "b288c214a61c07034936",
+    "url": "/DDSBlaze/static/js/2.29c49ba7.chunk.js"
+  },
+  {
+    "revision": "b7c7ed2a580115688d93140eb05c9c4e",
+    "url": "/DDSBlaze/static/media/default_profile_pic.b7c7ed2a.png"
+  },
+  {
+    "revision": "090327f790059fd254ceca5e52b507e7",
+    "url": "/DDSBlaze/static/media/phone_icon.090327f7.png"
+  },
+  {
+    "revision": "5bc7a81f114d690478abfbab0bcd47b4",
+    "url": "/DDSBlaze/static/media/location_icon.5bc7a81f.png"
+  },
+  {
+    "revision": "7a7040d60e2ea7efe1bff2f8e64ad411",
+    "url": "/DDSBlaze/index.html"
+  }
+];
