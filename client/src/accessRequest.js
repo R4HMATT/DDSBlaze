2b46e9c5a5c9a2c1a0bffa4b1966986b26c76110
Bilal Ahmed <bill.ahmed@mail.utoronto.ca>
2019-04-25 22:23:13 -0400

    removed all requirements of Connection.json

--- client/src/accessRequest.js
+++ client/src/accessRequest.js
@@ -4,22 +4,21 @@ import { Redirect } from 'react-router';
 
 //var exports = module.exports = {};
 
-let Connections = require('./Connection.json');
 
   var applicationConfig = {
         // this is the id of our website, registered in Azure Active Directory (it runs sharepoint)
-        clientID: Connections.sharepoint.client_id,
+        clientID: "adf1a6da-cc17-4863-885a-a2b6148aed42",
         //clientID: 'd162fed4-64b5-495d-8f10-e3bb4c0b9290',
         // the URL/URI used to get authorization form microsoft and grant us an Access Token
         // it features a tenant id (for our website's azure directory), the client id, and more required parameters (mostly defaulted)
         // this built from default values             <----------------TENANT ID---------->                 other parameters required ->                                                      
-        authority: `https://login.microsoftonline.com/bdcc68fe-2692-4c7a-b7fe-ca9ff4385193/oauth2/authorize?client_id=${Connections.sharepoint.client_id}&response_type=id_token+token&scope=openid&redirect_uri=https://localhost:3000/&state=12345&nonce=random&response_mode=form_post`,
+        authority: `https://login.microsoftonline.com/bdcc68fe-2692-4c7a-b7fe-ca9ff4385193/oauth2/authorize?client_id=adf1a6da-cc17-4863-885a-a2b6148aed42&response_type=id_token+token&scope=openid&redirect_uri=https://localhost:3000/&state=12345&nonce=random&response_mode=form_post`,
         // this is the scope for the access token request call
         // open id is a default value
         scope: ["Sites.Read.All"],
         // this gives u default user scopes? https://graph.microsoft.com/.default
         //spendpoint: "https://rahmnik.sharepoint.com/_api/web/lists/getbytitle('testlist')"
-        spendpoint: Connections.sharepoint.list_address
+        spendpoint: "https://graph.microsoft.com/v1.0/sites/rahmnik.sharepoint.com/lists/testlist/items?expand=fields"
         //"https://rahmnik.sharepoint.com/sites/DDSBlazeWFH/_api/read"
         // SP END point is our sharepoint website (more specifically a list) 
         // this hasnt been tested yet, so it could be wrong
